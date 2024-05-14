cf614547b4534a3ec96792904ce0629e49bb5196
Artem Yastremskiy <artekyas12@gmail.com>
2024-05-14 19:58:18 +0300

    update

--- visualizer.js
+++ visualizer.js
@@ -1,15 +1,58 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const fileInput = document.getElementById("audioFile");
-    const canvas = document.getElementById("audioCanvas");
-    const ctx = canvas.getContext('2d')
-    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-    let audioSource = null;
-    let analyser = null;
-    
+window.addEventListener("DOMContentLoaded", () => {
+  const fileInput = document.getElementById("audioFile");
+  const canvas = document.getElementById("audioCanvas");
+  const ctx = canvas.getContext("2d");
+  const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+  let audioSource = null;
+  let analyser = null;
 
-    fileInput.addEventListener('change', function () {
-        const files = this.files;
-        if (files.length === 0) return;
-        const file = files[0];
-    })
+  fileInput.addEventListener("change", function () {
+    const files = this.files;
+    if (files.length === 0) return;
+    const file = files[0];
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      if (audioSource) {
+        audioSource.disconnect();
+      }
+
+      audioContext.decodeAudioData(e.target.result, function (buffer) {
+        audioSource = audioContext.createBufferSource();
+        audioSource.buffer = buffer;
+        analyser = audioContext.createAnalyser();
+        analyser.fftSize = 256;
+        audioSource.connect(analyser);
+        analyser.connect(audioContext.destination);
+        audioSource.start();
+        animate();
+      });
+    };
+    reader.readAsArrayBuffer(file);
+  });
+  function animate() {
+    requestAnimationFrame(animate);
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+
+    analyser.getByteFrequencyData(dataArray);
+
+    ctx.fileStyle = 'rgb(0, 0, 0)';
+    ctx.fileRect(0, 0, canvas.width, canvas.height);
+    const barWidth = (canvas.width / bufferLength) * 2.5;
+
+    let barHeight;
+    let x = 0;
+
+    for (let i = 0; i < bufferLength; i++) {
+        barHeight = dataArray[i];
+        ctx.fileStyle = "rgb(" + (barHeight + 100) + ",50,50)";
+        ctx.fileRect(
+            x,
+            canvas.height - barHeight / 2,
+            barWidth,
+            barHeight / 2
+        );
+        x += barWidth / 2
+    }
+  };
 });
